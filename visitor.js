27a84b27dc3c9e542ddc93af8786b189f70109bf
Powfu! <75916532+tegzxy@users.noreply.github.com>
2023-11-03 02:45:19 +0700

    Delete visitor.js

--- visitor.js
+++
@@ -1,7 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://tegzxy.github.io/website-about-me/");
-xhr.responseType = "json";
-xhr.onload = function() {
-  document.querySelector('.footer-title').innerHTML = 'Total Visitor : ' + this.response.value;
-}
-xhr.send();
